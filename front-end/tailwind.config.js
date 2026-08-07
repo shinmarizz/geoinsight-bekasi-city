99ffc223c515c9acebd8c8877ac09df50979c73e
shinmarizz <adnanyusufh02@gmail.com>
2026-08-07 14:11:57 +0700

    update hero section and add tailwind.config.js

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content:['index.html'],
+    theme:{
+        extend:{
+            colors:{
+                primary:'.text-blue-600'
+            }
+    }},
+    plugins:{}
+}
