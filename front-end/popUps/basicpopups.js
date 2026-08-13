9d45904ff64c5b493f70560940f81ddfd281d283
shinmarizz <adnanyusufhartawan06@gmail.com>
2026-08-13 12:39:00 +0700

    update areaTool

--- front-end/popUps/basicpopups.js
+++ front-end/popUps/basicpopups.js
@@ -4,3 +4,5 @@ export function addPopup (map, event)
     const coordinate  = event.lngLat
     const longitude  = coordinate.lng.toFixed(2)
     const lattitude = coordinate.lat.toFixed(2)
+
+    const properties = 
