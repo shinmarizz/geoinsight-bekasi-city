0654d9f870739eab3ca54aea8ad5d305bc04a86d
shinmarizz <adnanyusufhartawan06@gmail.com>
2026-08-17 23:28:21 +0700

    app

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.get("/geoinsight")
+
 const PORT = 5001
 const server = app.listen(PORT, ()=>{
     console.log(`Berhasil ${PORT}`)
 })
 
+
+//GET POST PUT DELETE
+// https://localhost:5001/geoinsight
