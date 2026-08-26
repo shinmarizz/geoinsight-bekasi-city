9f82deb92bf08c4d08a9aa9f07993f2db8af4468
shinmarizz <adnanyusufhartawan06@gmail.com>
2026-08-26 14:07:28 +0700

    fix db

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -4,12 +4,15 @@ import { configDotenv } from "dotenv"
 configDotenv()
 
 const { Pool } = pg
-const pool = new Pool({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT
-})
+
+const pool = process.env.DATABASE_URL
+    ? new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
+    : new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+    })
 
 export default pool
